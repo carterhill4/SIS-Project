e7343b13f429e3a6331bc4ebe6b4fa508ad9b834
marika.holz <marika.holz@MU-C1-08.mullen.pvt.k12.co.us>
2021-10-27 12:11:44 -0600

    commit

--- SIS/src/AddingAndDeletingStudents.java
+++ SIS/src/AddingAndDeletingStudents.java
@@ -4,10 +4,13 @@ public class AddingAndDeletingStudents
 	{
 	public static void DeletingStudent()
 		{
-		
+		for(int i=0; i<25; i++)
+			{
+			System.out.println();	
+			}
 		}
 	public static void AddingStudent()
 		{
-		
+	
 		}
 	}
