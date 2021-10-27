5fa36b70bc79a56078b9a387932e81490361fa06
carter.hill <carter.hill@MU-C1-05.mullen.pvt.k12.co.us>
2021-10-27 12:10:18 -0600

    delted pojo

--- SIS/src/RunSIS.java
+++ SIS/src/RunSIS.java
@@ -1,24 +1,13 @@
+import java.util.ArrayList;
 
 public class RunSIS
 	{
-
+			static ArrayList <Student> listOfStudents = new ArrayList<Student>();
+			
 		public static void main(String[] args)
 			{
 
-			System.out.println("Hello World");
-				
-				
-				
-				
-				
-			System.out.println("Tika was here <3");
-			}
-	
-
-
-
-				
-				System.out.println("Carter typed this");
+				lisOfStudents.add(new Student("SISFile.txt"));
 
 
 			}
