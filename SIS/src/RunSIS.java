b418a09ae7214a1a3f0c714f34c2d6ca9ab3f77b
marika.holz <marika.holz@MU-C1-08.mullen.pvt.k12.co.us>
2021-10-27 11:48:13 -0600

    diff classes

--- SIS/src/RunSIS.java
+++ SIS/src/RunSIS.java
@@ -1,26 +1,12 @@
 
 public class RunSIS
 	{
+	public static void main(String[] args)
+		{
 
-		public static void main(String[] args)
-			{
 
-			System.out.println("Hello World");
-				
-				
-				
-				
-				
-			System.out.println("Tika was here <3");
-			}
+
+		}
 	
-
-
-
-				
-				System.out.println("Carter typed this");
-
-
-			}
 	
 	}
